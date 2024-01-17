9ee18f0ad93c1f4ea56ffb3682e35f8c0e8a3d0d
kemuniku <kemuni9@gmail.com>
2024-01-17 17:08:54 +0900

    レート推移を比較するためのプログラム

---
+++ rate.py
@@ -0,0 +1,15 @@
+import datetime
+import requests
+import matplotlib.pyplot as plt
+usernames = input("ユーザー名をカンマ区切りで入力してください:").split(",")
+users = {uname : requests.get("https://atcoder.jp/users/"+uname+"/history/json").json() for uname in usernames}
+user_rates = {uname : [users[uname][i]["NewRating"] for i in range(len(users[uname]))] for uname in usernames}
+user_times = {uname : [datetime.datetime.strptime((users[uname][i]["EndTime"]).split("T")[0], '%Y-%m-%d')  for i in range(len(users[uname]))] for uname in usernames}
+plt.figure()
+for uname in usernames:
+    plt.plot(user_times[uname], user_rates[uname],label=uname)
+plt.legend()
+plt.gcf().autofmt_xdate() 
+plt.yticks([i * 400 for i in range(13) if (i-1)*400 <= max(max(user_rates[uname] for uname in usernames))])
+plt.grid(which = "major", axis = "y", color = "green", alpha = 0.8,linestyle = "--", linewidth = 1)
+plt.show()
